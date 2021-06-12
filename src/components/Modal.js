0c7e990db03ae66b3feabf355c108c55e4c66c9e
unknown <a-f-r@hotmail.es>
2021-06-12 23:09:27 +0200

    Use portals to move the rendered modal to backdrop-root and overlay-root divs

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,26 +1,46 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
-const Backdrop = function () {};
+const Backdrop = function (props) {
+  const clickHandler = function () {
+    props.onClick(true);
+  };
+  return <div onClick={clickHandler} className="overlay"></div>;
+};
+
+const ModalOverlay = function (props) {
+  const clickHandler = function () {
+    props.onClick(true);
+  };
+  return (
+    <div className="modal">
+      <div className="h2-div">
+        <h2 className="h2">Invalid input</h2>
+      </div>
+      <p className="margin-p">
+        Please enter a valid name and age (non-empty values).
+      </p>
+      <button onClick={clickHandler} className="right">
+        Okay
+      </button>
+    </div>
+  );
+};
 
 function Modal(props) {
   const clickHandler = function () {
     props.onClick(true);
   };
-
   return (
     <React.Fragment>
-      <div className="modal">
-        <div className="h2-div">
-          <h2 className="h2">Invalid input</h2>
-        </div>
-        <p className="margin-p">
-          Please enter a valid name and age (non-empty values).
-        </p>
-        <button onClick={clickHandler} className="right">
-          Okay
-        </button>
-      </div>
-      <div onClick={clickHandler} className="overlay"></div>
+      {ReactDOM.createPortal(
+        <Backdrop onClick={clickHandler} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay onClick={clickHandler} />,
+        document.getElementById("overlay-root")
+      )}
     </React.Fragment>
   );
 }
